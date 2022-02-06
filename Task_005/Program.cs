05070a2809d9f586d4cb9d40335a494ba7d16b5d
Juliya <yuliya_2808@mail.ru>
2022-02-06 21:54:26 +0300

    Task_005

---
+++ Task_005/Program.cs
@@ -0,0 +1,24 @@
+﻿// 5.	Найти максимальное из трех чисел.
+
+int a1 = 10;
+int a2 = 12;
+int a3 = 25;
+if (a1 > a2)
+{
+   if (a1 > a3)
+   {
+       Console.WriteLine("max: " + a1);
+   }
+}
+else if (a2 > a3)
+{
+    Console.WriteLine("max: " + a2);
+}
+else if (a2 < a3)
+{
+    Console.WriteLine("max: " + a3);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
