d15c02d4d84493e1ac9f4da6b4968ab9351c5dfb
Juliya <yuliya_2808@mail.ru>
2022-02-06 23:37:16 +0300

    Task_007

---
+++ Task_007/Program.cs
@@ -0,0 +1,15 @@
+﻿// 7.	Показать числа от -N до N.
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+int N = num1;
+
+while (N <= num2)
+{
+    Console.Write(N);
+    N++;
+}
+
