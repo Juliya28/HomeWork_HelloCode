b104baadfda7d87af90fe71d0f3f040736f706cb
Juliya <yuliya_2808@mail.ru>
2022-02-06 23:49:06 +0300

    Task_008

---
+++ Task_008/Program.cs
@@ -0,0 +1,11 @@
+﻿// 8.	Показать четные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int N = 1;
+while (N <= num)
+{
+    if(N % 2 == 0)
+    Console.WriteLine(N);
+    N++;
+}
