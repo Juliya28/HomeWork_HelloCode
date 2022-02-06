be91daa2c9e2c3004c5d0460dda521dcefd8f43c
Juliya <yuliya_2808@mail.ru>
2022-02-06 21:17:58 +0300

    Task_003

---
+++ Task_003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Даны два числа. Показать большее и меньшее число.
+
+int a1 = 8;
+int a2 = 10;
+if (a1 > a2)
+{
+    Console.WriteLine("Большее: " + a1);
+    Console.WriteLine("Меньшее: " + a2);
+}
+else if (a1 < a2)
+{
+    Console.WriteLine("Большее: " + a2);
+    Console.WriteLine("Меньшее: " + a1);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
+
