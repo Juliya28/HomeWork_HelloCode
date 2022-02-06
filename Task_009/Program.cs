f2dcd39c31f96f4ca7ec739a75131af4b9bac329
Juliya <yuliya_2808@mail.ru>
2022-02-07 00:44:25 +0300

    Task_009

---
+++ Task_009/Program.cs
@@ -0,0 +1,8 @@
+﻿// 9.	Показать последнюю цифру трёхзначного числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+string a1 = Console.ReadLine();
+int b1 = int.Parse(a1);
+int result = b1 % 10;
+Console.WriteLine(result);
+
