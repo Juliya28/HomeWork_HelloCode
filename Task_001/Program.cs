0f6882f3c55a49ee4d06ee6719e3d12343f7b4e3
Juliya <yuliya_2808@mail.ru>
2022-02-06 18:55:20 +0300

    Task_oo1

---
+++ Task_001/Program.cs
@@ -0,0 +1,7 @@
+﻿// 1.	Вывести квадрат числа.
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int num = int.Parse(number);
+int result = num * num;
+Console.WriteLine(result);
