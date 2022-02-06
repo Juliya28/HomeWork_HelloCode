2b5f986cd34d26f7ecb0572bc86e6458ceec2e15
Juliya <yuliya_2808@mail.ru>
2022-02-07 01:37:29 +0300

    Task_012

---
+++ Task_012/Program.cs
@@ -0,0 +1,12 @@
+﻿// 12.	Удалить вторую цифру трёхзначного числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+string num = Console.ReadLine();
+int number = int.Parse(num);
+
+int a = number % 10;
+int b = number / 10 % 10;
+int c = number / 100 % 10;
+int result = c * 10 + a;
+
+Console.WriteLine(result);
