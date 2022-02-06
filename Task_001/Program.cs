770af2056045eb43bc59b699b46f0076fe051f10
Juliya <yuliya_2808@mail.ru>
2022-02-06 19:15:42 +0300

    Task_oo1_new info

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,7 +1,9 @@
 ﻿// 1.	Вывести квадрат числа.
 
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите число: ");
 string number = Console.ReadLine();
 int num = int.Parse(number);
 int result = num * num;
 Console.WriteLine(result);
+
+
