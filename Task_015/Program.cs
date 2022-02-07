33aa35e66307b7ef7386ee135e351930732885e3
Juliya <yuliya_2808@mail.ru>
2022-02-08 00:39:16 +0300

    Task_015

---
+++ Task_015/Program.cs
@@ -0,0 +1,13 @@
+﻿// 15.	Дано число. Проверить кратно ли оно 7 и 23.
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine(num + " Кратно");
+}
+else
+{
+    Console.WriteLine(num + " Не кратно");
+}
