3112b4d3208b01a3be81679077f659ce8ef55fb2
Juliya <yuliya_2808@mail.ru>
2022-02-08 00:18:10 +0300

    Task_014

---
+++ Task_014/Program.cs
@@ -0,0 +1,14 @@
+﻿// 14.	Найти третью цифру числа или сообщить, что её нет.
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int number = num / 100 % 10;
+
+if (number > 0)
+{
+    Console.WriteLine(number);
+}
+else 
+{
+    Console.WriteLine("Третья цифра отсутствует");
+}
