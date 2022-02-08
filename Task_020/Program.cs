829096bf62fb24215310fa6d405cc6aeeac189ce
Juliya <yuliya_2808@mail.ru>
2022-02-08 17:52:14 +0300

    Task_020 New

--- Task_020/Program.cs
+++ Task_020/Program.cs
@@ -9,17 +9,17 @@ int y = 0;
 
 if (num == 1)
 {
-    Console.WriteLine("Диапазон I четверти: {x > 0 && y > 0}");
+    Console.WriteLine("Диапазон I четверти: {x > 0 и y > 0}");
 }
 else if (num == 2)
 {
-    Console.WriteLine("Диапазон II четверти: {x < 0 && y > 0}");
+    Console.WriteLine("Диапазон II четверти: {x < 0 и y > 0}");
 }
 else if (num == 3)
 {
-    Console.WriteLine("Диапазон III четверти: {x < 0 && y < 0}");
+    Console.WriteLine("Диапазон III четверти: {x < 0 и y < 0}");
 }
 else if (num == 4)
 {
-    Console.WriteLine("Диапазон IV четверти: {x > 0 && y < 0}");
+    Console.WriteLine("Диапазон IV четверти: {x > 0 и y < 0}");
 }
