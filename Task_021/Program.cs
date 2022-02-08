e1fc06377edcfdb2ec1a9e7168a6613043a23473
Juliya <yuliya_2808@mail.ru>
2022-02-08 18:13:04 +0300

    Task_021

---
+++ Task_021/Program.cs
@@ -0,0 +1,19 @@
+﻿// 21.	Программа проверяет пятизначное число на палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int a1 = number % 10;
+int a2 = number / 10 % 10;
+int a3 = number / 100 % 10;
+int a4 = number / 1000 % 10;
+int a5 = number / 10000;
+
+if (a1 == a5 && a2 == a4)
+{
+    Console.WriteLine("Является полиндромом");
+}
+else
+{
+    Console.WriteLine("Не является полиндромом");
+}
