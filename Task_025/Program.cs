7e1a755a11e6e6dcf1f644d8f9dbd239e4627fac
Juliya <yuliya_2808@mail.ru>
2022-02-10 15:41:02 +0300

    new info

---
+++ Task_025/Program.cs
@@ -0,0 +1,20 @@
+﻿// 25.	Найти сумму чисел от 1 до А.
+
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int[] array = new int[A];
+
+//int summ = 0;
+
+for (int i = 1; i <= array.Length; i++)
+{
+    // int summ = summ + array[i];
+    Console.WriteLine(array[i]);
+    //Console.WriteLine(i);
+}
+
+
+    // result = i + (i+1);
+    // Console.WriteLine(i);
