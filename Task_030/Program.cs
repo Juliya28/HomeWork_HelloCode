d8670fed3f16e598c3352440c1f940382955a424
Juliya <yuliya_2808@mail.ru>
2022-02-15 00:14:25 +0300

    Task_030

---
+++ Task_030/Program.cs
@@ -0,0 +1,15 @@
+﻿// 30.	Показать кубы чисел, заканчивающихся на четную цифру.
+
+//Console.WriteLine("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
+int num = new Random().Next(1, 100);
+Console.WriteLine(num);
+
+Console.WriteLine("Кубы чисел, заканчивающихся на четную цифру: ");
+
+ for (int i = 1; i <= num; i++)
+ {
+     if ((i*i*i) % 2 == 0)
+    Console.Write((i*i*i) + "\t");
+ }
+ Console.WriteLine();
