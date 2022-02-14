47262ed23d44b798ac2c8070ffefcd52ee22a809
Juliya <yuliya_2808@mail.ru>
2022-02-14 16:36:02 +0300

    Task_027 Last info

---
+++ Task_027/Program.cs
@@ -0,0 +1,12 @@
+﻿// 27.	Определить количество цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int result = 0;
+while (num > 0)
+{
+    num = num / 10;
+    result++;
+}
+Console.WriteLine(result);
