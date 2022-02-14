3ebda10c7eeff93f6fe6d5fbb3ad0af21df0557d
Juliya <yuliya_2808@mail.ru>
2022-02-14 16:51:54 +0300

    Task_029

---
+++ Task_029/Program.cs
@@ -0,0 +1,21 @@
+﻿// 29.	Написать программу вычисления произведения чисел от 1 до N.
+
+Console.WriteLine("Введите число А: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Числа от 1 до А: " + "\t");
+Console.WriteLine();
+
+int summ = 1;
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($"{i}" + "\t");
+}
+Console.WriteLine();
+for (int i = 1; i <= num; i++)
+{
+    summ = summ * i;
+}
+Console.WriteLine();
+Console.WriteLine("Сумма чисел от 1 до А: " + summ);
+Console.WriteLine();
