e65cde77bdecbe220edda379cd92fb97850c7c00
Juliya <yuliya_2808@mail.ru>
2022-02-15 21:32:19 +0300

    Task_033

---
+++ Task_033/Program.cs
@@ -0,0 +1,35 @@
+﻿// 33.	Задать массив из 12 элементов, заполненных числами из [0,9]. 
+// Найти сумму положительных/отрицательных элементов массива.
+
+int [] array = new int [12];
+
+void FillArray (int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = new Random(). Next(-500, 501);
+    }
+}
+
+FillArray (array);
+
+void PrintArray (int[] arr)
+{
+    for (int j = 0; j < arr.Length; j++)
+    {
+        Console.Write(arr[j] + " ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray (array);
+
+int positive = 0;
+int negative = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) positive += 1;
+    else negative += 1;
+}
+Console.WriteLine($"В заданном массиве {positive} положительных чисел и {negative} отрицательных чисел");
