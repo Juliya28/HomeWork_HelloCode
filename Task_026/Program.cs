1357c7120cabbb3cacab565794728ff4e828876e
Juliya <yuliya_2808@mail.ru>
2022-02-15 14:52:21 +0300

    Task_026 New info

--- Task_026/Program.cs
+++ Task_026/Program.cs
@@ -2,15 +2,14 @@
 
 Console.WriteLine("Введите число: ");
 int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine();
 Console.Write("Введите число, в степенень которого хотите возвести первое число: ");
 Console.WriteLine();
 int num2 = int.Parse(Console.ReadLine());
 
-int result = 0;
-for (int i = 0; i <= num2; i++)
+int result = 1;
+for (int i = 1; i <= num2; i++)
 {
-    result = num1*num1;
+    result *= num1;
 }
-Console.WriteLine();
 Console.WriteLine($"Число возведенное в степень: " + result);
+Console.WriteLine();
