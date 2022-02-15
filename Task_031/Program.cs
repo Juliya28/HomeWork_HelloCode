5bace7bb39828a67af4a530a4dff8bc2881dacf2
Juliya <yuliya_2808@mail.ru>
2022-02-15 15:34:15 +0300

    Task_031

---
+++ Task_031/Program.cs
@@ -0,0 +1,10 @@
+﻿// 31.	Задать массив из 8 элементов и вывести их на экран.
+
+int[] array = { 4, 5, 6, 7, 6 , 8, 9, 3 };
+int index = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
