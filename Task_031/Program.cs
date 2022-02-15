86c12544acb5c207e53c459a9d1733a231d195ef
Juliya <yuliya_2808@mail.ru>
2022-02-15 15:47:42 +0300

    Task_028 New changing

--- Task_031/Program.cs
+++ Task_031/Program.cs
@@ -1,7 +1,15 @@
 ﻿// 31.	Задать массив из 8 элементов и вывести их на экран.
 
+/* void Print (int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write(mass[i] + " ");
+    }
+} */
+
+
 int[] array = { 4, 5, 6, 7, 6 , 8, 9, 3 };
-int index = 0;
 
 for (int i = 0; i < array.Length; i++)
 {
