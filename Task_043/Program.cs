5c296d5087379dca1e85ddf40fea83a50566fb63
Juliya <yuliya_2808@mail.ru>
2022-02-20 14:48:33 +0300

    Task_043

---
+++ Task_043/Program.cs
@@ -0,0 +1,16 @@
+﻿// 43.	Написать программу преобразования десятичного числа в двоичное.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+string result = "";
+while (a != 0)
+{
+    result = (a % 2).ToString() + result;
+    a = a / 2;
+    
+}
+Console.Write(result);
+Console.WriteLine();
