c7f40470eb471728ea418581d0094c3d404afb1d
Juliya <yuliya_2808@mail.ru>
2022-02-20 17:07:20 +0300

    Task_042

---
+++ Task_042/Program.cs
@@ -0,0 +1,12 @@
+﻿// 42.	Определить сколько чисел больше 0 введено с клавиатуры.
+
+Console.WriteLine("Введите числа через пробел: ");
+string num = Console.ReadLine();
+string[] array = num.Split(' ');
+
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (int.Parse(array[i]) > 0) result++;
+}
+Console.WriteLine("Количество чисел больше 0: " + result);
