17ccc935062fa952178ae3df8f00856cdf158963
Juliya <yuliya_2808@mail.ru>
2022-02-20 15:35:37 +0300

    Task_045

---
+++ Task_045/Program.cs
@@ -0,0 +1,17 @@
+﻿// 45.	Показать числа Фибоначчи.
+
+double Fibonachi(int n)
+{
+    if (n < 3)
+    {
+        return 1;
+    }
+    return Fibonachi(n - 1) + Fibonachi(n - 2);
+}
+int num = 8;
+Console.WriteLine(Fibonachi(num));
+
+for (int i = 1; i < num; i++)
+{
+    Console.WriteLine(Fibonachi(i));
+}
