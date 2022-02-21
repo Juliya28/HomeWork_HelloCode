deed39e2d289bce1edc57c485ed108c4f93d98ce
Juliya <yuliya_2808@mail.ru>
2022-02-21 21:56:44 +0300

    Task_052

---
+++ Task_052/Program.cs
@@ -0,0 +1,47 @@
+﻿// 52.	В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты.
+
+Console.Clear();
+Console.Write("Введите число, которое даст количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число, которое даст количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+int[,] array = new int [m, n];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+    Console.WriteLine();
+    }
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(0); i+=2)
+{
+    for (int j = 0; j < array.GetLength(1); j+=2)
+    {
+        array[i, j] *= array[i, j];
+    }
+}
+PrintArray(array);
+Console.WriteLine();
