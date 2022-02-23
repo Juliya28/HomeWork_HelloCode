8893feddede1d219c322f4e083a452f2e05d6913
Juliya <yuliya_2808@mail.ru>
2022-02-23 16:42:10 +0300

    Task_051

---
+++ Task_051/Program.cs
@@ -0,0 +1,52 @@
+﻿// 51.	Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+
+Console.Clear();
+Console.Write("Введите число, которое даст количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число, которое даст количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+int[,] array = new int [m, n];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+int[,] MethodSum()
+{
+    int[,] massiv = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            massiv[i, j] = i + j;
+        }
+    }
+    return massiv;
+}
+
+
+FillArray(array);
+int[,] massiv = MethodSum();
+PrintArray(massiv);
