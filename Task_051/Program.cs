407e387b1f61305a7b28086cef7a7d89802f17b7
Juliya <yuliya_2808@mail.ru>
2022-02-23 16:44:48 +0300

    Task_051 New

--- Task_051/Program.cs
+++ Task_051/Program.cs
@@ -9,17 +9,6 @@ int n = int.Parse(Console.ReadLine());
 Console.WriteLine();
 int[,] array = new int [m, n];
 
-void FillArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
-    }
-}
-
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -46,7 +35,5 @@ int[,] MethodSum()
     return massiv;
 }
 
-
-FillArray(array);
 int[,] massiv = MethodSum();
 PrintArray(massiv);
