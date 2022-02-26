9ca7aba72547c74e114fe53e0f34d3dd65d32be3
Juliya <yuliya_2808@mail.ru>
2022-02-26 15:47:53 +0300

    Task_061

---
+++ Task_061/Program.cs
@@ -0,0 +1,55 @@
+﻿// 61.	Найти произведение двух матриц.
+
+Console.Clear();
+Console.Write("Введите число, которое даст количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число, которое даст количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+int[,] array = new int [m, n];
+int [,] massiv = new int [m, n];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+    Console.WriteLine();
+    }
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("===================");
+FillArray(massiv);
+PrintArray(massiv);
+Console.WriteLine("===================");
+
+int[,] matrix = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = array[i, j] * massiv[i, j];
+        
+    }
+}
+Console.WriteLine("Произведение двух матриц: ");
+Console.WriteLine();
+PrintArray(matrix);
+Console.WriteLine();
