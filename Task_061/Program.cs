e58b5657f6d6fb1e12d3c02b6d8a5bd8cc71e589
Juliya <yuliya_2808@mail.ru>
2022-03-01 22:02:25 +0300

    Task_061

--- Task_061/Program.cs
+++ Task_061/Program.cs
@@ -1,15 +1,20 @@
 ﻿// 61.	Найти произведение двух матриц.
 
 Console.Clear();
-Console.Write("Введите число, которое даст количество строк: ");
+Console.Write("Введите число, которое даст количество строк первой матрицы: ");
 int m = int.Parse(Console.ReadLine());
-Console.Write("Введите число, которое даст количество столбцов: ");
+Console.Write("Введите число, которое даст количество столбцов первой матрицы: ");
 int n = int.Parse(Console.ReadLine());
+Console.Clear();
+Console.Write("Введите число, которое даст количество строк второй матрицы: ");
+int m1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число, которое даст количество столбцов второй матрицы: ");
+int n1 = int.Parse(Console.ReadLine());
 
 Console.WriteLine();
 
 int[,] array = new int [m, n];
-int [,] massiv = new int [m, n];
+int [,] massiv = new int [m1, n1];
 
 void FillArray(int[,] array)
 {
@@ -41,15 +46,29 @@ PrintArray(massiv);
 Console.WriteLine("===================");
 
 int[,] matrix = new int[m, n];
-for (int i = 0; i < m; i++)
+if (n != m1)
 {
-    for (int j = 0; j < n; j++)
-    {
-        matrix[i, j] = array[i, j] * massiv[i, j];
-        
-    }
+    Console.WriteLine("Введите одинаковое количество строк второй матрицы и столбцов первой матрицы!");
+    Console.WriteLine();
 }
-Console.WriteLine("Произведение двух матриц: ");
-Console.WriteLine();
-PrintArray(matrix);
-Console.WriteLine();
+else
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n1; j++)
+        {
+            int sum = 0;
+            for (int k = 0 ; k < n; k++)
+            {
+                sum += array[i, k] * massiv[k, j];
+            }
+            matrix[i, j] = sum;
+        
+        }
+    }
+    Console.WriteLine("Произведение двух матриц: ");
+    Console.WriteLine();
+    PrintArray(matrix);
+    Console.WriteLine();
+}
+
