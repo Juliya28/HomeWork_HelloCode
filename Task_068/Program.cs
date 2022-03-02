0056a03551dc7e3230a8d17ee9405ff125e4d4f1
Juliya <yuliya_2808@mail.ru>
2022-03-02 20:44:40 +0300

    Task_068

---
+++ Task_068/Program.cs
@@ -0,0 +1,21 @@
+﻿// 68.	Показать натуральные числа от M до N, N и M заданы.
+
+Console.Clear();
+void List (int m, int n)
+{
+    if (m > n)
+    {
+        List(m - 1, n);
+    }
+    else if (m < n)
+    {
+        List(m + 1, n);
+    }
+    Console.Write(m + "  ");
+}
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+List(n, m);
+Console.WriteLine();
