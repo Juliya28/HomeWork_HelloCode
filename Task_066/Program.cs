e644b24331933f72ce4e4abfa1fb07a1cfb801bb
Juliya <yuliya_2808@mail.ru>
2022-03-02 22:30:16 +0300

    Task_066

---
+++ Task_066/Program.cs
@@ -0,0 +1,13 @@
+﻿// 66.	Показать натуральные числа от 1 до N, N задано.
+Console.Clear();
+int List (int n)
+{
+    if (n < 1) return 0;
+
+    Console.Write(n + "  ");
+    return List(n - 1);
+}
+Console.WriteLine("Введите границу интервала: ");
+int n = int.Parse(Console.ReadLine());
+List(n);
+Console.WriteLine();
