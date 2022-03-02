fc1714e7b06c63ba47d5ee40a938208a466af4b0
Juliya <yuliya_2808@mail.ru>
2022-03-02 22:29:32 +0300

    Task_070

---
+++ Task_070/Program.cs
@@ -0,0 +1,18 @@
+﻿// 70.	Найти сумму цифр числа.
+
+Console.Clear();
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+
+int summ = 0;
+int SummFind(int num)
+{
+    if (num % 10 > 0)
+    {
+        summ += num % 10;
+        SummFind(num / 10);
+    }
+    return summ;
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма цифр числа = {SummFind(m)}");
