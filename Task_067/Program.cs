a067df254ef1ed6d5b97ed40054677862eae105c
Juliya <yuliya_2808@mail.ru>
2022-03-02 20:38:15 +0300

    Task_067

---
+++ Task_067/Program.cs
@@ -0,0 +1,15 @@
+﻿// 67.	Показать натуральные числа от N до 1, N задано.
+
+Console.Clear();
+int Rec (int n)
+{
+    if (n == 0) return 0;
+
+    Console.Write(n + "  ");
+    return Rec(n - 1);
+}
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Rec(n);
+Console.WriteLine();
