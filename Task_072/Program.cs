745dffcaa739da2ffab1df3e2b2abd0d7cc7a621
Juliya <yuliya_2808@mail.ru>
2022-03-02 23:06:59 +0300

    Task_072

---
+++ Task_072/Program.cs
@@ -0,0 +1,16 @@
+﻿// 72.	Написать программу возведения числа А в целую стень B.
+
+Console.Clear();
+Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+int PowNum(int a, int b)
+{
+    if (b == 1) return a;
+    else return a * PowNum(a, b - 1);
+}
+Console.WriteLine("===================");
+Console.WriteLine($"Число {a} возведено в степень {b} = {PowNum(a, b)}");
+Console.WriteLine();
