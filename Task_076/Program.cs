eb9cdc56b55ebf706a2143a39883e4007af7f557
Juliya <yuliya_2808@mail.ru>
2022-03-07 12:44:43 +0300

    Task_076

---
+++ Task_076/Program.cs
@@ -0,0 +1,27 @@
+﻿// 76.	Есть число N. Скольно групп M, можно получить при разбиении всех чисел на группы, 
+// так чтобы в одной группе все числа были взаимно просты.
+// Например для N = 50, M получается 6
+// Задача: найти M при заданном N и получить одно из разбиений на группы N ≤ 10²⁰.
+
+
+Console.Clear();
+Console.Write("Введите N: ");
+int num = Int32.Parse(Console.ReadLine());
+
+int group = 0;
+void SeveralGroup(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        if (i == Math.Pow(2, group))
+        {
+            Console.WriteLine();
+            group++;
+            Console.Write($"Группа {group}: ");
+        }
+            Console.Write($"{i} ");
+    }
+        Console.WriteLine();
+        Console.WriteLine();
+}
+SeveralGroup(num);
